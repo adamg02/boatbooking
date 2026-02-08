1e880ed131b821bf72338f26bd393b9859e82c65
Adam Gill <adam.gill@hawkcreative.com>
2026-02-08 19:02:06 +0000

    fix oauth redirect.

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,12 +4,13 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = await getSupabaseClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  // Redirect to boats page
-  return NextResponse.redirect(new URL("/boats", requestUrl.origin));
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(`${origin}/boats`);
 }
